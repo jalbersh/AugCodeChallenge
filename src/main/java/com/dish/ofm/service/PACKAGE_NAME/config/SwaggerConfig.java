48fcebe3a1efc1d60e2b02ecc0fedee09bc2b5e1
James Scovern and Lucy Follansbee <pair+james.scovern@dish.com+lucy.follansbee@dish.com@pivotal.io>
2017-03-09 13:03:04 -0700

    update gradle licensing so it will build and edit swagger config

--- src/main/java/com/dish/ofm/service/PACKAGE_NAME/config/SwaggerConfig.java
+++ src/main/java/com/dish/ofm/service/PACKAGE_NAME/config/SwaggerConfig.java
@@ -2,37 +2,29 @@ package com.dish.ofm.service.PACKAGE_NAME.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
     @Bean
-    public Docket newsApi() {
+    public Docket docket() {
         return new Docket(DocumentationType.SWAGGER_2)
-            .groupName("service-adapters")
-            .apiInfo(apiInfo())
-            .select()
-            .paths(regex(".*/REST_ENDPOINT.*"))
-            .build();
+            .useDefaultResponseMessages(false);
     }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-            .title("PROJECT_TITLE")
-            .description("PROJECT_TITLE Dish Offer Management")
-            .termsOfServiceUrl("http://terms of service")
-            .contact(new Contact("Service Adapters", "", "OFMServiceAdapterTeam@dish.com"))
-            .license("Dish Internal Use Only")
-            .licenseUrl("http://dishurl copyright")
-            .version("0.1")
-            .build();
+    @Controller
+    @Profile(value = "prod")
+    public static class SwaggerRedirectController {
+        @RequestMapping("/swagger-ui.html")
+        public ResponseEntity swagger() {
+            throw new ResourceNotFoundException();
+        }
     }
 }
