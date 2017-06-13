ad0b286e13fe657375872ff4ae1508a0ca7f2dc5
james albersheim <james@albersheim.com>
2017-06-13 14:28:07 -0600

    [US31782][S][Patch] Use logging interceptor from common-utils

--- src/main/java/com/dish/ofm/service/PACKAGE_NAME/config/APPLICATION_NAMEConfig.java
+++ src/main/java/com/dish/ofm/service/PACKAGE_NAME/config/APPLICATION_NAMEConfig.java
@@ -1,12 +1,9 @@
 package com.dish.ofm.service.PACKAGE_NAME.config;
 
 import com.dish.core.metrics.health.RestServiceHealthStatusIndicator;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.BufferingClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.LinkedList;
@@ -14,13 +11,6 @@ import java.util.List;
 
 @Configuration
 public class APPLICATION_NAMEConfig {
-    @Bean
-    public RestTemplate restTemplate() {
-        RestTemplate rt = new RestTemplate(new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()));
-        rt.getInterceptors().add(new LoggingClientHttpRequestInterceptor());
-        return rt;
-    }
-
     @Bean
     public HealthIndicator getRestHealthIndicator() {
         List<String> uris = new LinkedList<>();
